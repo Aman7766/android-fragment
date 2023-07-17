2b8a69c253a4104b41c4369d705bd5b313561193
Amandeep <amankumar283@gmail.com>
2023-07-16 23:45:31 -0400

    Java Fragment Code Added

--- app/src/main/java/com/amandeep/fragment/MainActivity.java
+++ app/src/main/java/com/amandeep/fragment/MainActivity.java
@@ -3,12 +3,37 @@ package com.amandeep.fragment;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.FrameLayout;
 
 public class MainActivity extends AppCompatActivity {
+    FrameLayout frameLayout;
+    Button btn1,btn2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        frameLayout=findViewById(R.id.frml);
+        btn1=findViewById(R.id.btn1);
+        btn2=findViewById(R.id.btn2);
+
+        btn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        btn2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
     }
+
+
 }
